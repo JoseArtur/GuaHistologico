4e71281ac9c761c6eac2bb3d68a05738febde81a
José Artur <joseartura1.2@gmail.com>
2024-02-24 12:40:21 +0000

    removed unused lines

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,9 @@
 import React from 'react';
 import Header from './components/Header';
 import { supabase } from "@/lib/supabase";
-import { useEffect, useState } from 'react';
-import DataType from './utils/DataType';
 import Post from './components/Post';
 
 const HomePage: React.FC = () => {
-  const [data, setData] = useState<DataType[]>([]);
-
-  const getNewView = async () => {
-    const {data, error} = await supabase.from('post').select('*');
-    if (error) {
-      console.log(error);
-    }
-    if (data) {
-      setData(data);
-    }
-  }
-
-  useEffect(() => {
-    getNewView();
-  }, []);
-
   return (
     <>
        <div className="home-page">
@@ -35,17 +17,7 @@ const HomePage: React.FC = () => {
       />
       {/* Other components and elements */}
     </div>
-      {/* { <div>
-        {data.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <img src={item.primary_image} alt={item.title} />
-            {item.secondary_images.map((image, index) => (
-              <img key={index} src={image} alt={item.title} />
-            ))}
-          </div>
-        ))}
-      </div> } */}
+      {/* Other components and elements */}
     </>
   );
 };
