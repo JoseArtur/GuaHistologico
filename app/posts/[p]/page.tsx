4871c1a37c3ca152b17345cea2c8ad25d06cdd0e
Vinícius Corrêa <viniciusmc@inditex.com>
2024-07-03 10:04:15 +0200

    given a proper redesign

--- app/posts/[p]/page.tsx
+++ app/posts/[p]/page.tsx
@@ -7,38 +7,23 @@ import Carousel from "@itseasy21/react-elastic-carousel";
 import Image from "next/image";
 import titles from "@/app/utils/titles.json";
 import { TitlesType } from "@/app/utils/TitlesType";
-import { ClipLoader } from "react-spinners"; // Add this line to import the spinner
+import { ClipLoader } from "react-spinners";
 
 export default function Page({ params }: { params: { p: string } }) {
   const pathTranslations: TitlesType = titles as TitlesType;
-
   const title = pathTranslations[params.p] || params.p;
   const [data, setData] = useState<DataType[]>([]);
-  const [photoOpacities, setPhotoOpacities] = useState<Map<number, number[]>>(
-    new Map()
-  );
+  const [photoOpacities, setPhotoOpacities] = useState<Map<number, number[]>>(new Map());
   const [isMobile, setIsMobile] = useState(false);
-  const [loading, setLoading] = useState(true); // Add this state for loading
+  const [loading, setLoading] = useState(true);
 
-  const handleAnimation = (
-    animationClass: string,
-    index: number,
-    indexParent: number
-  ) => {
+  const handleAnimation = (index: number, indexParent: number) => {
     setPhotoOpacities((prevOpacities) => {
       const currentOpacities = prevOpacities.get(indexParent) || [];
-      const newOpacities = [...currentOpacities]; // create a new array
-      newOpacities[index] = newOpacities[index] === 1 ? 0 : 1; // toggle opacity
-      const updatedOpacities = new Map(prevOpacities); // create a new Map
-      updatedOpacities.set(indexParent, newOpacities);
-      return updatedOpacities;
+      const newOpacities = [...currentOpacities];
+      newOpacities[index] = newOpacities[index] === 1 ? 0 : 1;
+      return new Map(prevOpacities).set(indexParent, newOpacities);
     });
-  };
-
-  const handleReset = () => {
-    setPhotoOpacities(
-      new Map(Array.from(photoOpacities.keys()).map((key) => [key, [0]]))
-    ); // reset opacity of all photos to 0 when reset button is clicked
   };
 
   const getNewView = async () => {
@@ -46,273 +31,191 @@ export default function Page({ params }: { params: { p: string } }) {
       .from("post")
       .select("*")
       .eq("title", params.p);
-    if (error) {
-      console.log(error);
-    }
+    if (error) console.log(error);
     if (data) {
       setData(data);
-      const initialOpacities = new Map();
-
-      for (let i = 0; i < data.length; i++) {
-        initialOpacities.set(
-          i,
-          new Array(data[i].secondary_images?.length).fill(0)
-        );
-      }
-      setPhotoOpacities(initialOpacities); // initialize opacity map with 0s
+      setPhotoOpacities(new Map(data.map((_, i) => [i, new Array(data[i].secondary_images?.length).fill(0)])));
     }
-    setLoading(false); // Set loading to false after data is fetched
+    setLoading(false);
   };
 
   useEffect(() => {
     getNewView();
-  }, [params.p]);
-
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 640);
-    };
-
+    const checkMobile = () => setIsMobile(window.innerWidth <= 768);
     checkMobile();
     window.addEventListener("resize", checkMobile);
-
-    return () => {
-      window.removeEventListener("resize", checkMobile);
-    };
-  }, []);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, [params.p]);
 
   return (
-    <>
-      <div className="post-page mb-96">
-        <h1 className="text-center text-2xl xl:text-5xl font-bold mb-20 border-gray-200 pt-40">
-          {title}
-        </h1>
-        {loading ? (
-          <div className="flex justify-center items-center h-96">
-            <ClipLoader size={50} color={"#123abc"} loading={loading} />
-          </div>
-        ) : (
-          <Carousel
-            className="carousel"
-            isRTL={false}
-            showArrows={!isMobile}
-            enableSwipe={isMobile}
-          >
-            {data.map((post, index) => (
-              <div key={index} className="carousel-item">
-                <div className="button-container">
-                  {post.secondary_images?.map((image, i) => (
-                    <div key={i} className="toggle-switch">
-                      <input
-                        type="checkbox"
-                        id={`toggle${index}-${i + 1}`}
-                        className="toggle-input"
-                        onChange={() =>
-                          handleAnimation(`animate${index}-${i + 1}`, i, index)
-                        }
-                      />
-                      <label
-                        htmlFor={`toggle${index}-${i + 1}`}
-                        className="toggle-label"
-                      ></label>
-                      <div className="toggle-name">
-                        {post.secondary_images_names[i]}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className={`relative w-full h-auto sm:w-full sm:h-auto`}>
-                  <Image
-                    className="photo-1 object-cover object-center"
-                    layout="fill"
-                    src={post.primary_image}
-                    alt={post.title}
-                  />
-                  {post.secondary_images?.map((image, i) => (
-                    <Image
-                      key={i}
-                      className={`photo${index}-${
-                        i + 1
-                      } object-cover object-center`}
-                      layout="fill"
-                      style={{
-                        opacity: (photoOpacities.get(index) || [0])[i] || 0,
-                      }}
-                      src={image}
-                      alt={post.title}
-                    />
-                  ))}
-                </div>
-              </div>
-            ))}
-          </Carousel>
-        )}
-      </div>
-      <style jsx>{`
-        .toggle-switch {
-          position: relative;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          width: 30vw;
-          height: 4vh;
-          margin-bottom: 0.2em;
-        }
-
-        .toggle-input {
-          opacity: 0;
-          width: 0;
-          height: 0;
-        }
-
-        .toggle-name {
-          font-size: 0.7em;
-          margin-left: 4px;
-          text-wrap: pretty;
-        }
-
-        .toggle-label {
-          position: relative;
-          cursor: pointer;
-          background-color: #ccc;
-          transition: 0.4s;
-          border-radius: 3em;
-          width: 60px;
-          height: 34px;
-          margin-right: 10px;
-          margin-left: 10px;
-        }
-
-        .toggle-input:checked + .toggle-label {
-          background-color: #2196f3;
-        }
-
-        .toggle-label:before {
-          position: absolute;
-          content: "";
-          height: 26px;
-          width: 26px;
-          left: 4px;
-          bottom: 4px;
-          background-color: white;
-          transition: 0.4s;
-          border-radius: 50%;
-          border: 1px solid #ccc;
-        }
-
-        .toggle-input:checked + .toggle-label:before {
-          transform: translateX(26px);
-        }
-
-        .carousel-item {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-        }
-
-        .post-page {
-          margin-top: 5.625vh;
-          margin-left: 5.5vw;
-          margin-right: 5.5vw;
-          text-align: center;
-          font-size: 2em;
-        }
-
-        .relative {
-          position: relative;
-          margin-left: 3.625vw;
-          width: 1000px;
-          height: 500px;
-        }
-
-        @media (min-width: 640px) {
-          .relative {
-            width: 1000px;
-            height: 600px;
-          }
-        }
-        @media (min-width: 4050px) {
-          .toggle-name {
-            font-size: 1.5em;
-            margin-left: 4px;
-            text-wrap: pretty;
-          }
-          .relative {
-            width: 1800px;
-            height: 900px;
-          }
-        }
-        @media (min-width: 5050px) {
-          .relative {
-            width: 2000px;
-            height: 1000px;
-          }
-          .toggle-name {
-            font-size: 1.7em;
-            margin-left: 4px;
-            text-wrap: pretty;
-          }
-        }
-        @media (max-width: 2000px) {
-          .button-container {
-            margin-top: 2em;
-          }
-
-          .toggle-switch {
-            flex-direction: row;
-            align-items: center;
-            justify-content: flex-start;
-            width: 100%;
-            height: auto;
-          }
-
-          .toggle-name {
-            font-size: 0.7em;
-            margin-left: 4px;
-            text-wrap: pretty;
-          }
-
-          .toggle-label {
-            width: 40px;
-            height: 20px;
-          }
-
-          .carousel-item {
-            flex-direction: column-reverse;
-          }
-
-          .post-page {
-            margin-top: 2.8125vh;
-            margin-left: 2.75vw;
-            margin-right: 2.75vw;
-            font-size: 1em;
-            margin-bottom: 12.8125vh;
-          }
-
-          .relative {
-            margin-left: 1.8125vw;
-            width: 24em;
-            height: 12em;
-          }
-
-          .toggle-label:before {
-            position: absolute;
-            content: "";
-            height: 20px;
-            width: 20px;
-            left: 1px;
-            bottom: 0px;
-            background-color: white;
-            transition: 0.4s;
-            border-radius: 50%;
-            border: 1px solid #ccc;
-          }
-
-          .toggle-input:checked + .toggle-label:before {
-            transform: translateX(20px);
-          }
-        }
-      `}</style>
-    </>
+    <div className="post-page">
+      <h1 className="title">{title}</h1>
+      {loading ? (
+        <div className="loader">
+          <ClipLoader size={50} color={"#123abc"} loading={loading} />
+        </div>
+      ) : (
+        <Carousel className="carousel" isRTL={false} showArrows={!isMobile} enableSwipe={isMobile}>
+          {data.map((post, index) => (
+  <div key={index} className="carousel-item">
+  <div className="content-wrapper">
+    <div className="toggle-container">
+      {post.secondary_images?.map((_, i) => (
+        <div key={i} className="toggle-switch">
+          <input
+            type="checkbox"
+            id={`toggle${index}-${i + 1}`}
+            className="toggle-input"
+            onChange={() => handleAnimation(i, index)}
+          />
+          <label htmlFor={`toggle${index}-${i + 1}`} className="toggle-label"></label>
+          <span className="toggle-name" title={post.secondary_images_names[i]}>
+            {post.secondary_images_names[i]}
+          </span>
+        </div>
+      ))}
+    </div>
+    <div className="image-container">
+      <Image
+        className="photo"
+        layout="fill"
+        objectFit="contain"
+        src={post.primary_image}
+        alt={post.title}
+      />
+      {post.secondary_images?.map((image, i) => (
+        <Image
+          key={i}
+          className="photo"
+          layout="fill"
+          objectFit="contain"
+          style={{ opacity: (photoOpacities.get(index) || [])[i] || 0 }}
+          src={image}
+          alt={post.title}
+        />
+      ))}
+    </div>
+  </div>
+</div>
+          ))}
+        </Carousel>
+      )}
+<style jsx global>{`
+  .post-page {
+    max-width: 1200px;
+    margin: 5rem auto;
+    padding: 0 1rem;
+  }
+  .title {
+    font-size: 3rem;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  .carousel-item {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  .content-wrapper {
+    display: flex;
+    width: 100%;
+    gap: 2rem;
+  }
+  .toggle-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 250px;
+    flex-shrink: 0;
+  }
+  .toggle-switch {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+  .toggle-input {
+    display: none;
+  }
+  .toggle-label {
+    width: 60px;
+    height: 34px;
+    background-color: #ccc;
+    border-radius: 34px;
+    cursor: pointer;
+    position: relative;
+    transition: background-color 0.3s;
+  }
+  .toggle-label:before {
+    content: '';
+    position: absolute;
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+    top: 4px;
+    left: 4px;
+    background-color: white;
+    transition: 0.3s;
+  }
+  .toggle-input:checked + .toggle-label {
+    background-color: purple;
+  }
+  .toggle-input:checked + .toggle-label:before {
+    transform: translateX(26px);
+  }
+  .toggle-name {
+    margin-left: 1rem;
+    font-size: 1rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 150px;
+  }
+  .image-container {
+    position: relative;
+    width: calc(100% - 250px - 2rem);
+    height: 0;
+    padding-bottom: 56.25%; /* 16:9 aspect ratio */
+  }
+  .photo {
+    object-fit: contain;
+  }
+  @media (max-width: 768px) {
+    .content-wrapper {
+      flex-direction: column;
+    }
+    .toggle-container, .image-container {
+      width: 100%;
+    }
+    .toggle-container {
+      order: 2;
+      margin-top: 1rem;
+    }
+    .image-container {
+      order: 1;
+      padding-bottom: 75%; /* 4:3 aspect ratio for mobile */
+    }
+    .title {
+      font-size: 2rem;
+    }
+    .toggle-label {
+      width: 50px;
+      height: 28px;
+    }
+    .toggle-label:before {
+      width: 22px;
+      height: 22px;
+      top: 3px;
+      left: 3px;
+    }
+    .toggle-input:checked + .toggle-label:before {
+      transform: translateX(22px);
+    }
+    .toggle-name {
+      font-size: 0.9rem;
+    }
+  }
+`}</style>
+    </div>
   );
 }
